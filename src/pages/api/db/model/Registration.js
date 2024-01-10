574198e6146938b34298be373299860b0f9ec27b
sajith p j <sajith@vass.co.in>
2024-01-10 14:34:55 +0530

    added timestamp

--- src/pages/api/db/model/Registration.js
+++ src/pages/api/db/model/Registration.js
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
-const registrationSchema = new mongoose.Schema({
-  regId: {
-    type: String,
-    default: 10000,
+const registrationSchema = new mongoose.Schema(
+  {
+    regId: {
+      type: String,
+      default: 10000,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    company: {
+      type: String,
+    },
+    district: {
+      type: String,
+    },
+    isVerified: { type: Boolean, default: false },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-  },
-  category: {
-    type: String,
-  },
-  company: {
-    type: String,
-  },
-  district: {
-    type: String,
-  },
-  isVerified: { type: Boolean, default: false },
-});
+  { timestamps: true }
+);
 const Registration =
   mongoose.models.Registration ||
   mongoose.model("Registration", registrationSchema);

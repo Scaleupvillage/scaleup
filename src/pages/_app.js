c9f0f2785ac4dfa5ed47b24f65f2fd5cdba50222
sajith p j <sajith@vass.co.in>
2024-01-10 15:15:47 +0530

    fbpixel adding

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,23 @@
 import "@/styles/globals.css";
 import "@/styles/table.scss";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("889871215930367"); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
   return (
     <>
       <ToastContainer />

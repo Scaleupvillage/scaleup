3a69ab9e3c1b776785cb07c007f661337bce73df
sajith p j <sajith@vass.co.in>
2024-01-20 21:06:54 +0530

    changed phottos

--- src/constants/speakers.js
+++ src/constants/speakers.js
@@ -22,48 +22,48 @@ import miandad from "@/assets/images/speakers/miandad.webp";
 import hassan from "@/assets/images/speakers/hassan.webp";
 import majeed from "@/assets/images/speakers/majeed.webp";
 import nadeemsafran from "@/assets/images/speakers/nadeemsafran.webp";
-import vinod from "@/assets/images/speakers/vinod.png";
-import shamsudeen from "@/assets/images/speakers/shamsudeen.png";
-import faisal from "@/assets/images/speakers/faisal.png";
-import tomthomas from "@/assets/images/speakers/tomthomas.png";
-import abdulnazar from "@/assets/images/speakers/abdulnazar.png";
-import basim from "@/assets/images/speakers/basim.png";
-import ebadu from "@/assets/images/speakers/ebadu.png";
-import shameem from "@/assets/images/speakers/shameem.png";
-import anil from "@/assets/images/speakers/anil.png";
-import roshan from "@/assets/images/speakers/roshan.png";
-import suman from "@/assets/images/speakers/suman.png";
-import anvar from "@/assets/images/speakers/anvar.png";
-import mehar from "@/assets/images/speakers/mehar.png";
-import nadeem from "@/assets/images/speakers/nadeem.png";
-import pvunnikrishnan from "@/assets/images/speakers/pvunnikrishnan.png";
-import jayashankar from "@/assets/images/speakers/jayashankar.png";
-import abhijit from "@/assets/images/speakers/abhijit.png";
-import ramees from "@/assets/images/speakers/ramees.png";
-import bpnazar from "@/assets/images/speakers/bpnazar.png";
-import razak from "@/assets/images/speakers/razak.png";
-import mathewjoseph from "@/assets/images/speakers/mathewjoseph.png";
-import dana from "@/assets/images/speakers/dana.png";
-import kalesh from "@/assets/images/speakers/kalesh.png";
-import mathukutty from "@/assets/images/speakers/mathukutty.png";
-import jogin from "@/assets/images/speakers/jogin.png";
-import mufeed from "@/assets/images/speakers/mufeed.png";
-import atif from "@/assets/images/speakers/atif.png";
-import thomasgeorge from "@/assets/images/speakers/thomasgeorge.png";
-import najiya from "@/assets/images/speakers/najiya.png";
-import shameena from "@/assets/images/speakers/shameena.png";
-import minshad from "@/assets/images/speakers/minshad.png";
-import arranjit from "@/assets/images/speakers/arranjit.png";
-import ajmaltp from "@/assets/images/speakers/ajmaltp.png";
-import deena from "@/assets/images/speakers/deena.png";
-import ashokkurian from "@/assets/images/speakers/ashokkurian.png";
-import balagopal from "@/assets/images/speakers/balagopal.png";
-import akshay from "@/assets/images/speakers/akshay.png";
-import sheelan from "@/assets/images/speakers/sheelan.png";
-import riyas from "@/assets/images/speakers/riyas.png";
-import azaadmoopan from "@/assets/images/speakers/azaadmoopan.png";
-import farish from "@/assets/images/speakers/farish.png";
-import umer from "@/assets/images/speakers/umer.png";
+import vinod from "@/assets/images/speakers/vinod.webp";
+import shamsudeen from "@/assets/images/speakers/shamsudeen.webp";
+import faisal from "@/assets/images/speakers/faisal.webp";
+import tomthomas from "@/assets/images/speakers/tomthomas.webp";
+import abdulnazar from "@/assets/images/speakers/abdulnazar.webp";
+import basim from "@/assets/images/speakers/basim.webp";
+import ebadu from "@/assets/images/speakers/ebadu.webp";
+import shameem from "@/assets/images/speakers/shameem.webp";
+import anil from "@/assets/images/speakers/anil.webp";
+import roshan from "@/assets/images/speakers/roshan.webp";
+import suman from "@/assets/images/speakers/suman.webp";
+import anvar from "@/assets/images/speakers/anvar.webp";
+import mehar from "@/assets/images/speakers/mehar.webp";
+import nadeem from "@/assets/images/speakers/nadeem.webp";
+import pvunnikrishnan from "@/assets/images/speakers/pvunnikrishnan.webp";
+import jayashankar from "@/assets/images/speakers/jayashankar.webp";
+import abhijit from "@/assets/images/speakers/abhijit.webp";
+import ramees from "@/assets/images/speakers/ramees.webp";
+import bpnazar from "@/assets/images/speakers/bpnazar.webp";
+import razak from "@/assets/images/speakers/razak.webp";
+import mathewjoseph from "@/assets/images/speakers/mathewjoseph.webp";
+import dana from "@/assets/images/speakers/dana.webp";
+import kalesh from "@/assets/images/speakers/kalesh.webp";
+import mathukutty from "@/assets/images/speakers/mathukutty.webp";
+import jogin from "@/assets/images/speakers/jogin.webp";
+import mufeed from "@/assets/images/speakers/mufeed.webp";
+import atif from "@/assets/images/speakers/atif.webp";
+import thomasgeorge from "@/assets/images/speakers/thomasgeorge.webp";
+import najiya from "@/assets/images/speakers/najiya.webp";
+import shameena from "@/assets/images/speakers/shameena.webp";
+import minshad from "@/assets/images/speakers/minshad.webp";
+import arranjit from "@/assets/images/speakers/arranjit.webp";
+import ajmaltp from "@/assets/images/speakers/ajmaltp.webp";
+import deena from "@/assets/images/speakers/deena.webp";
+import ashokkurian from "@/assets/images/speakers/ashokkurian.webp";
+import balagopal from "@/assets/images/speakers/balagopal.webp";
+import akshay from "@/assets/images/speakers/akshay.webp";
+import sheelan from "@/assets/images/speakers/sheelan.webp";
+import riyas from "@/assets/images/speakers/riyas.webp";
+import azaadmoopan from "@/assets/images/speakers/azaadmoopan.webp";
+import farish from "@/assets/images/speakers/farish.webp";
+import umer from "@/assets/images/speakers/umer.webp";
 
 // speakers background images
 // speaker bg
@@ -90,389 +90,389 @@ import bg20 from "@/assets/images/speakers/bg/bg20.png";
 
 const speakers = [
   {
-    image: "Minister.src",
+    image: Minister.src,
     role: "Minister for Industries, Law and Coir in the Government of Kerala",
     name: "P Rajeev",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "Kunjalikutty.src",
+    image: Kunjalikutty.src,
     role: "Former Minister for Industries, MLA Vengara",
     name: "P. K. Kunhalikutty",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "MLA.src",
+    image: MLA.src,
     role: "MLA Perinthalmanna",
     name: "Najeeb Kanthapuram",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "NavasMeeran.src",
+    image: NavasMeeran.src,
     role: "Chairman, Group Meeran",
     name: "Navas Meeran",
     captionBg: "  bg-primary-cyan ",
   },
   {
-    image: "AnishAchuthan.src",
+    image: AnishAchuthan.src,
     role: "Cofounder & CEO, Open Financial Technologies",
     name: "Anish Achuthan",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "HariKishore.src",
+    image: HariKishore.src,
     role: "Director, Industries and Commerce Department",
     name: "Harikishore S. IAS",
     captionBg: "bg-primary-blue",
   },
   {
-    image: "vinod.src",
+    image: vinod.src,
     role: "Distric Collector, Malappuram",
     name: "V R Vinod IAS",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "shamsudeen.src",
+    image: shamsudeen.src,
     role: "Chaiman & MD,Abate Group",
     name: "Dr.Shamsudheen",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "faisal.src",
+    image: faisal.src,
     role: "Dean, The B School",
     name: "Faisal P Syed",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "tomthomas.src",
+    image: tomthomas.src,
     role: "COO,Kerala Startup Mission",
     name: "Tom Thomas",
     captionBg: "bg-primary-blue",
   },
   {
-    image: "abdulnazar.src",
+    image: abdulnazar.src,
     role: "C0-Founder, Director at ID Fresh",
     name: "Abdul Nazar",
     captionBg: "bg-primary-purple",
   },
   {
-    image: "basim.src",
+    image: basim.src,
     role: "Basim's Plate",
     name: "Abdul Basim",
     captionBg: "bg-primary-cyan",
   },
   {
-    image: "deepak.src",
+    image: deepak.src,
     role: "CEO, Triplei",
     name: "Deepak H",
     captionBg: "  bg-primary-purple ",
   },
   // jobin is missing
   {
-    image: "nadeemsafran.src",
+    image: nadeemsafran.src,
     role: "CEO, Tharacart",
     name: "Nadeem Safran Kongath",
     captionBg: "bg-primary-cyan ",
   },
   {
-    image: "ebadu.src",
+    image: ebadu.src,
     role: "",
     name: "Ebadu Rahman",
     captionBg: "bg-primary-blue ",
   },
   {
-    image: "shameem.src",
+    image: shameem.src,
     role: "",
     name: "Shameem Rafeek",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "anil.src",
+    image: anil.src,
     role: "",
     name: "Anil Muhammed",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "roshan.src",
+    image: roshan.src,
     role: "",
     name: "R Roshan",
     captionBg: "bg-primary-blue ",
   },
 
   {
-    image: "anvar.src",
+    image: anvar.src,
     role: "Little Kites",
     name: "Anver Sadath",
     captionBg: "bg-primary-cyan ",
   },
   {
-    image: "mehar.src",
+    image: mehar.src,
     role: "Tinkerhub",
     name: "Mehar Moosa",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "nadeem.src",
+    image: nadeem.src,
     role: "Foxlab Marketspace",
     name: "Nadeem Ahmed",
     captionBg: "bg-primary-blue ",
   },
   {
-    image: "pvunnikrishnan.src",
+    image: pvunnikrishnan.src,
     role: "KDISC",
     name: "Dr. P V Unnikrishnan",
-    captionBg: "bg-primary-cyan ",
+    captionBg: "bg-primary-blue ",
   },
   {
-    image: "jayashankar.src",
+    image: jayashankar.src,
     role: "DCSMAT",
     name: "Dr. Jayashankar Prasad",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "abhijit.src",
+    image: abhijit.src,
     role: "Project DEFY",
     name: "Abhijit Sinha",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "ramees.src",
+    image: ramees.src,
     role: "Interval",
     name: "Ramees",
     captionBg: "bg-primary-cyan ",
   },
   {
-    image: "Tanooraa.src",
+    image: Tanooraa.src,
     role: "Founder, Zoul and Zera",
     name: "Tanooraa Swetha Menon",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "VimalGovind.src",
+    image: VimalGovind.src,
     role: "Co-Founder & CEO, Genrobotics ",
     name: "Vimal Govind MK",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "bpnazar.src",
+    image: bpnazar.src,
     role: "Fortune Logistic",
     name: "B P Nasar",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "razak.src",
+    image: razak.src,
     role: "VKC",
     name: "Razak",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "mathewjoseph.src",
+    image: mathewjoseph.src,
     role: "Fresh To Home",
     name: "Mathew Joseph",
-    captionBg: "  bg-primary-cyan ",
+    captionBg: "  bg-primary-blue ",
   },
   {
-    image: "dana.src",
+    image: dana.src,
     role: "",
     name: "Dana Raziq",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "kalesh.src",
+    image: kalesh.src,
     role: "",
     name: "Kalesh",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "mathukutty.src",
+    image: mathukutty.src,
     role: "",
     name: "Mathukutty",
     captionBg: "  bg-primary-cyan ",
   },
   {
-    image: "jogin.src",
+    image: jogin.src,
     role: "",
     name: "Jogin Francis",
     captionBg: "bg-primary-blue ",
   },
   {
-    image: "mufeed.src",
+    image: mufeed.src,
     role: "",
     name: "Mufeedh",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "atif.src",
+    image: atif.src,
     role: "",
     name: "Atif Muhammed",
     captionBg: "bg-primary-purple ",
   },
   {
-    image: "thomasgeorge.src",
+    image: thomasgeorge.src,
     role: "",
     name: "Dr. Thomas George",
     captionBg: "bg-primary-blue ",
   },
   {
-    image: "miandad.src",
+    image: miandad.src,
     role: "Chairman 33 Holdings Gloabal",
     name: "V P Muhammed Miandad",
-    captionBg: "  bg-primary-cyan ",
+    captionBg: "  bg-primary-blue ",
   },
   {
-    image: "najiya.src",
+    image: najiya.src,
     role: "Cart Ur Craft",
     name: "Najiya",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "shameena.src",
+    image: shameena.src,
     role: "Amaze Crochet",
     name: "Shameena",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "minshad.src",
+    image: minshad.src,
     role: "",
     name: "Minshad",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "arranjit.src",
+    image: arranjit.src,
     role: "Bramma Business Insights",
     name: "A R Ranjith",
-    captionBg: "  bg-primary-cyan ",
+    captionBg: "  bg-primary-blue ",
   },
   {
-    image: "ajmaltp.src",
+    image: ajmaltp.src,
     role: "Brand Buji",
     name: "Ajmal T P",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "UshaTitus.src",
+    image: UshaTitus.src,
     role: "Chairperson and MD, ASAP Kerala",
     name: "Dr Usha Titus",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "deena.src",
+    image: deena.src,
     role: "OPEN",
     name: "Deena Jacob",
     captionBg: "  bg-primary-cyan ",
   },
   {
-    image: "SanthoshGeorge.src",
+    image: SanthoshGeorge.src,
     role: " Founder and MD, Safari TV",
     name: "Santhosh George Kulangara",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "shreyasmv.src",
+    image: shreyasmv.src,
     role: "Managing Director, Mathrubhumi",
     name: "M V Sreyas Kumar",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "ashokkurian.src",
+    image: ashokkurian.src,
     role: "Kerala Startup Mission",
     name: "Ashok Kurian ",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "azaadmoopan.src",
+    image: azaadmoopan.src,
     role: "Aster MIMS",
     name: "Dr. Azad Moopan",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "farish.src",
+    image: farish.src,
     role: "Google",
     name: "Farish C V",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "umer.src",
+    image: umer.src,
     role: "Google",
     name: "Umer Abdusalam",
     captionBg: "  bg-primary-cyan ",
   },
   {
-    image: "sheelan.src",
+    image: sheelan.src,
     role: "Mi Zone Park",
     name: "Sheelan Sukumaran",
     captionBg: "  bg-primary-purple ",
   },
   {
-    image: "balagopal.src",
+    image: balagopal.src,
     role: "Kerala Startup Mission",
     name: "M Balagopal ",
     captionBg: "  bg-primary-cyan ",
   },
   {
-    image: "akshay.src",
+    image: akshay.src,
     role: "Eduport",
     name: "Akshay",
     captionBg: "  bg-primary-blue ",
   },
   {
-    image: "riyas.src",
+    image: riyas.src,
     role: "Kerala Startup Mission",
     name: "P M Riyas",
     captionBg: "bg-primary-purple ",
   },
 
   {
-    image: "Gopinath.src",
+    image: Gopinath.src,
     role: "Vice Chancellor, Kerala University of Digital Sciences, Innovation and Technology",
     name: "Saji Gopinath",
     captionBg: "  bg-primary-purple ",
   },
   // {
-  //   image: "Musthafa.src",
+  //   image: Musthafa.src,
   //   role: "CEO, iD Fresh Food India PVT. LTD.",
   //   name: "PC Musthafa",
   //   captionBg: "  bg-primary-purple ",
   // },
   // {
-  //   image: "ShamlalAhamad.src",
+  //   image: ShamlalAhamad.src,
   //   role: "MD International Relations Malabar Gold & Diamonds",
   //   name: "Shamlal Ahamed MP",
   //   captionBg: "  bg-primary-purple ",
   // },
   // {
-  //   image: "AnoopAmbika.src",
+  //   image: AnoopAmbika.src,
   //   role: "CEO, Kerala Startup Mission",
   //   name: "Anoop Ambika",
   //   captionBg: "  bg-primary-purple ",
   // },
   // {
-  //   image: "MabelChacko.src",
+  //   image: MabelChacko.src,
   //   role: "Co-Founder & COO, Open Financial Technologies",
   //   name: "Mabel Chacko",
   //   captionBg: "  bg-primary-purple ",
   // },
   // {
-  //   image: "IbnuJala.src",
+  //   image: IbnuJala.src,
   //   role: "Founder & CEO, Morfin Versity",
   //   name: "Ibnu Jala",
   //   captionBg: "  bg-primary-blue ",
   // },
   // {
-  //   image: "majeed.src",
+  //   image: majeed.src,
   //   role: "Saif Belhasa Auomotive, Dubai, UAE",
   //   name: "Majeed C K",
   //   captionBg: "  bg-primary-purple ",
   // },
   // {
-  //   image: "hassan.src",
+  //   image: hassan.src,
   //   role: "Chairman & CEO, Flora Hospitality Dubai. MD, SBK Real Estate Dubai",
   //   name: "Hassan V A",
   //   captionBg: "  bg-primary-cyan ",
   // },
 
   // {
-  //   image: "MuhammedMusthafa.src",
+  //   image: MuhammedMusthafa.src,
   //   role: "Founder & CEO, Edroots International",
   //   name: "Muhammed Musthafa Koori",
   //   captionBg: "  bg-primary-purple ",
